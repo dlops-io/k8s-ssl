741cea5c093510d7fb652f211956605fba47c6f4
shivasj <shivasj@gmail.com>
2025-01-13 14:38:15 -0500

    template pipfile and service

---
+++ src/api-service/api/service.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+# Setup FastAPI app
+app = FastAPI(title="API Server", description="API Server", version="v1")
+
+# Enable CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=False,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Routes
+@app.get("/")
+async def get_index():
+    return {"message": "Welcome to AC215"}
+
+# Additional routers here
+
